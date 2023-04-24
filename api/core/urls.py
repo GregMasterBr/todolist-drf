7dbe00c776e76ed6682de6d28a7656a758bd571a
GregorioQueiroz-at-030282358746 <gregorio.queiroz@facens.br>
2023-04-24 14:36:45 -0300

    adicionado a possibilidade dos métodos PUT e DELETE

--- api/core/urls.py
+++ api/core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from api.core.views import todo_list
+from api.core.views import todo_list, todo_detail_change_delete
 
 urlpatterns = [
-    path('', todo_list, name="todo_list")
+    path('', todo_list, name="todo_list"),
+    path('<int:pk>/', todo_detail_change_delete, name="todo_detail_change_delete"),
 
 ]
